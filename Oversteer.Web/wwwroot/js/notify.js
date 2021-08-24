3ce11df8ab2b18ab02dfdacf5a5d085607147800
AngelVasilev <defiend@abv.bg>
2021-08-24 23:51:00 +0300

    Last one.

--- Oversteer.Web/wwwroot/js/notify.js
+++ Oversteer.Web/wwwroot/js/notify.js
@@ -3,7 +3,7 @@
     .build();
 
 connection.on("NotifyOrders", (message) => {
-    customAlert(message, 10000);
+    customAlert(message);
 });
 
 
@@ -11,16 +11,8 @@ connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-function customAlert(msg, duration) {
-    var styler = document.createElement("div");
-    styler.classList.add("justify-content-around");
-    styler.classList.add("container");
-    styler.classList.add("rounded");
-    styler.classList.add("border-info");
-    styler.setAttribute("style", "width:auto;height:auto;top:50%;left:40%;background-color:#2196F3;color:white;text-align: center");
-    styler.innerHTML = "<h1>" + msg + "</h1>";
-    setTimeout(function () {
-        styler.parentNode.removeChild(styler);
-    }, duration);
-    document.body.appendChild(styler);
+function customAlert(msg) {
+    $("#messageContainer").html(msg);
+    $("#messageContainer").slideDown(2000);
+    setTimeout('$("#messageContainer").slideUp(2000);', 5000);
 }
